a535852137278b30cc9effc72685b045cb112cb0
stephenliu8964 <stephen.liu@ecodev.vic.gov.au>
2020-08-10 22:43:59 +1000

    - added route for Login and Signup page - created pages for Login and Signup - added database connect - added the User model - added functionality to login as a User - added functionality to signup a User - added js file to setup the Passport required for login strategy - modified the header view file to dynamically display current username - included more package dependency

---
+++ params/params.js
@@ -0,0 +1,5 @@
+const DATABASECONNECTION = 'mongodb+srv://dianqiangliu:hHz36tP8Vf6m@cluster0.hee10.mongodb.net/watchesapp?retryWrites=true&w=majority';
+
+module.exports = {
+    DATABASECONNECTION
+}
