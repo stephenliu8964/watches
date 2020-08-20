14d0234d8d7ea38a1b78961f0c6421ecb6e13e76
stephenliu8964 <stephen.liu@ecodev.vic.gov.au>
2020-08-20 21:03:16 +1000

    - added ability to display error and info message on the header ejs - added middleware to secure routes

--- routes/web/index.js
+++ routes/web/index.js
@@ -5,6 +5,8 @@ var router = express.Router();
 //TODO:: add in error and info
 router.use(function(req, res, next){
     res.locals.currentUser = req.user;
+    res.locals.error = req.flash('error');
+    res.locals.info = req.flash('info');
 
     next();
 });
